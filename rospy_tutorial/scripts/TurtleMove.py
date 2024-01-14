ff3ee3933eb3cb66aa84e1aae7b122cc10ee39a7
Feargal Murphy <ferg-mu@users.noreply.github.com>
2024-01-14 14:04:29 +0100

    turtle pose subscriber and twist publisher

---
+++ rospy_tutorial/scripts/TurtleMove.py
@@ -0,0 +1,73 @@
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from math import pow, atan2, sqrt
+
+class TurtleBot:
+
+    def __init__(self):
+        rospy.init_node('turtlebot_controller', anonymous=True)
+
+        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
+        self.pose = Pose()
+        self.rate = rospy.Rate(10)
+
+    def update_pose(self, data):
+        """Callback function which is called when a new message of type Pose is received by the subscriber."""
+        self.pose = data
+        self.pose.x = round(self.pose.x, 4)
+        self.pose.y = round(self.pose.y, 4)
+
+    def euclidean_distance(self, goal_pose):
+        """Euclidean distance between current pose and the goal."""
+        return sqrt(pow((float(goal_pose.x) - self.pose.x), 2) + pow((float(goal_pose.y) - self.pose.y), 2))
+
+    def steering_angle(self, goal_pose):
+        """Angle which the turtle needs to rotate to align with the goal position."""
+        return atan2(float(goal_pose.y) - self.pose.y, float(goal_pose.x) - self.pose.x)
+    
+    def linear_vel(self, goal_pose, constant=1.5):
+        """Velocity as a linear function of the distance to be moved. Velocity = K * Distance"""
+        return constant * self.euclidean_distance(goal_pose)
+
+    def angular_vel(self, goal_pose, constant=6):
+        """Angular velocity at which the turtle has to rotate to align with goal position"""
+        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
+
+    def move2goal(self):
+        """Moves the turtle to the goal."""
+        goal_pose = Pose()
+
+        goal_pose.x = input("Set your x goal: ")
+        goal_pose.y = input("Set your y goal: ")
+
+        distance_tolerance = float(input("Set your tolerance: "))
+
+        vel_msg = Twist()
+
+        while self.euclidean_distance(goal_pose) >= distance_tolerance:
+            
+            vel_msg.linear.x = self.linear_vel(goal_pose)
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
+            
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            vel_msg.angular.z = self.angular_vel(goal_pose)
+            
+            self.velocity_publisher.publish(vel_msg)
+            
+            self.rate.sleep()
+            
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        x = TurtleBot()
+        x.move2goal()
+    except rospy.ROSInterruptException:
+        pass
